61930d918748524986968ba0c3abe9fc524fab59
Bibek Paudel <bibek_poudel2@outlook.com>
2018-11-16 13:59:46 +0545

    summary calculation formulas

---
+++ Assignment1/Summary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Summary
+    {
+    
+        public static decimal Average(List<string> value)
+        {
+            var sum = 0;
+
+            foreach (var val in value)
+            {
+                sum = sum + Int32.Parse(val);
+            }
+
+            decimal result  = (sum / value.Count);
+            return result;
+        }
+
+        public static double Sum(List<string> value)
+        {
+           double sum = 0;
+
+            foreach (var val in value)
+            {
+                sum = sum + Double.Parse(val);
+            } 
+
+            return sum;
+        }
+        public static int Max(List<string> value)
+        {
+            int maxVal = 0;
+
+            for (int i = 0; i < value.Count; i++)
+            {
+                if (maxVal < Int32.Parse(value.ElementAt(i)))
+                {
+                    maxVal = Int32.Parse(value.ElementAt(i));
+                }
+            }
+
+            return maxVal;
+        }
+
+        public static int Min(List<string> value)
+        {
+            int minVal = Int32.Parse(value.ElementAt(0));
+
+            for (int i = 0; i < value.Count; i++)
+            {
+                if (minVal > Int32.Parse(value.ElementAt(i)))
+                {
+                    minVal = Int32.Parse(value.ElementAt(i));
+                }
+            }
+
+            return minVal;
+        }
+
+    }
+}
