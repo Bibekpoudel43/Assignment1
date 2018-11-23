61d1165533b5aed25c2aa103fd514be6c2347214
Bibek Paudel <bibek_poudel2@outlook.com>
2018-11-23 09:54:46 +0545

    Working on a individual graph

---
+++ Assignment1/IndividualGraph.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Assignment1
+{
+    partial class IndividualGraph
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zedGraphControl11 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl3 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl4 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl5 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl11
+            // 
+            this.zedGraphControl11.IsShowPointValues = false;
+            this.zedGraphControl11.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl11.Name = "zedGraphControl11";
+            this.zedGraphControl11.PointValueFormat = "G";
+            this.zedGraphControl11.Size = new System.Drawing.Size(806, 147);
+            this.zedGraphControl11.TabIndex = 0;
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.IsShowPointValues = false;
+            this.zedGraphControl2.Location = new System.Drawing.Point(12, 165);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.PointValueFormat = "G";
+            this.zedGraphControl2.Size = new System.Drawing.Size(806, 134);
+            this.zedGraphControl2.TabIndex = 1;
+            this.zedGraphControl2.Resize += new System.EventHandler(this.zedGraphControl2_Resize);
+            // 
+            // zedGraphControl3
+            // 
+            this.zedGraphControl3.IsShowPointValues = false;
+            this.zedGraphControl3.Location = new System.Drawing.Point(12, 305);
+            this.zedGraphControl3.Name = "zedGraphControl3";
+            this.zedGraphControl3.PointValueFormat = "G";
+            this.zedGraphControl3.Size = new System.Drawing.Size(806, 151);
+            this.zedGraphControl3.TabIndex = 2;
+            // 
+            // zedGraphControl4
+            // 
+            this.zedGraphControl4.IsShowPointValues = false;
+            this.zedGraphControl4.Location = new System.Drawing.Point(12, 462);
+            this.zedGraphControl4.Name = "zedGraphControl4";
+            this.zedGraphControl4.PointValueFormat = "G";
+            this.zedGraphControl4.Size = new System.Drawing.Size(806, 151);
+            this.zedGraphControl4.TabIndex = 3;
+            // 
+            // zedGraphControl5
+            // 
+            this.zedGraphControl5.IsShowPointValues = false;
+            this.zedGraphControl5.Location = new System.Drawing.Point(12, 619);
+            this.zedGraphControl5.Name = "zedGraphControl5";
+            this.zedGraphControl5.PointValueFormat = "G";
+            this.zedGraphControl5.Size = new System.Drawing.Size(806, 118);
+            this.zedGraphControl5.TabIndex = 4;
+            // 
+            // IndividualGraph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 749);
+            this.Controls.Add(this.zedGraphControl5);
+            this.Controls.Add(this.zedGraphControl4);
+            this.Controls.Add(this.zedGraphControl3);
+            this.Controls.Add(this.zedGraphControl2);
+            this.Controls.Add(this.zedGraphControl11);
+            this.Name = "IndividualGraph";
+            this.Text = "SummaryGraph";
+            this.Load += new System.EventHandler(this.IndividualGraph_Load);
+            this.Resize += new System.EventHandler(this.IndividualGraph_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl11;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private ZedGraph.ZedGraphControl zedGraphControl3;
+        private ZedGraph.ZedGraphControl zedGraphControl4;
+        private ZedGraph.ZedGraphControl zedGraphControl5;
+    }
+}
---
+++ Assignment1/IndividualGraph.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Assignment1
+{
+    public partial class IndividualGraph : Form
+    {
+        List<string> _heartRate = null;
+        List<string> _speed = null;
+        List<string> _cadence = null;
+        List<string> _altitude = null;
+        List<string> _power = null;
+        
+
+        public IndividualGraph(List<string> heartRate, List<string> speed, List<string> cadence, List<string> altitude, List<string> power)
+        {
+            InitializeComponent();
+            _heartRate = heartRate;
+            _speed = speed;
+            _cadence = cadence;
+            _altitude = altitude;
+            _power = power;
+        }
+
+        private PointPairList buildPointPairList(int[] value)
+        {
+            PointPairList pr = new PointPairList();
+            for (int i = 0; i < value.Count(); i++)
+            {
+                pr.Add(i, (value[i]));
+            }
+            return pr;
+        }
+        private void plotGraph()
+        {
+            GraphPane myPane = zedGraphControl11.GraphPane;
+
+            // Set the Titles
+            myPane.Title = "Analysis of HeartRate";
+            myPane.XAxis.Title = "Time in seconds";
+            myPane.YAxis.Title = "Value";
+            /* myPane.XAxis.Scale.MajorStep = 50;
+             myPane.YAxis.Scale.Mag = 0;
+             myPane.XAxis.Scale.Max = 1000;*/
+
+            PointPairList heartPairList = new PointPairList();
+            List<int> hr = _heartRate.Select(s => Convert.ToInt32(s)).ToList();
+            heartPairList = buildPointPairList(hr.ToArray());
+            LineItem heartCurve = myPane.AddCurve("HeartRate",
+            heartPairList, Color.Red, SymbolType.None);
+            zedGraphControl11.AxisChange();
+
+
+            GraphPane myPane2 = zedGraphControl2.GraphPane;
+
+            // Set the Titles
+            myPane2.Title = "Analysis of Speed";
+            myPane2.XAxis.Title = "Time in seconds";
+            myPane2.YAxis.Title = "Value";
+            PointPairList speedPairList = new PointPairList();
+            List<int> c2 = _speed.Select(s => Convert.ToInt32(s)).ToList();
+            speedPairList = buildPointPairList(c2.ToArray());
+            LineItem speedCurve = myPane.AddCurve("Speed",
+            speedPairList, Color.Blue, SymbolType.None);
+            zedGraphControl2.AxisChange();
+
+
+            GraphPane myPane3 = zedGraphControl3.GraphPane;
+
+            // Set the Titles
+            myPane3.Title = "Analysis of Cadence";
+            myPane3.XAxis.Title = "Time in seconds";
+            myPane3.YAxis.Title = "Value";
+            PointPairList cadencePairList = new PointPairList();
+            List<int> c3 = _cadence.Select(s => Convert.ToInt32(s)).ToList();
+            cadencePairList = buildPointPairList(c3.ToArray());
+            LineItem cadenceCurve = myPane.AddCurve("Cadence",
+            cadencePairList, Color.Black, SymbolType.None);
+            zedGraphControl3.AxisChange();
+
+            GraphPane myPane4 = zedGraphControl4.GraphPane;
+
+            // Set the Titles
+            myPane4.Title = "Analysis of Power";
+            myPane4.XAxis.Title = "Time in seconds";
+            myPane4.YAxis.Title = "Value";
+            PointPairList powerPairList = new PointPairList();
+            List<int> c4 = _power.Select(s => Convert.ToInt32(s)).ToList();
+            powerPairList = buildPointPairList(c4.ToArray());
+            LineItem powerCurve = myPane.AddCurve("Power",
+            speedPairList, Color.Cyan, SymbolType.None);
+            zedGraphControl4.AxisChange();
+
+
+            GraphPane myPane5 = zedGraphControl5.GraphPane;
+
+            // Set the Titles
+            myPane5.Title = "Analysis of Altitude";
+            myPane5.XAxis.Title = "Time in seconds";
+            myPane5.YAxis.Title = "Value";
+            PointPairList altitudePairList = new PointPairList();   
+            List<int> c5 = _altitude.Select(s => Convert.ToInt32(s)).ToList();
+            altitudePairList = buildPointPairList(c5.ToArray());
+            LineItem altitudeCurve = myPane.AddCurve("Altitude",
+                  altitudePairList, Color.Crimson, SymbolType.None
+                  );
+            zedGraphControl.AxisChange();
+        }
+
+        private void SetSize()
+        {
+            zedGraphControl11.Location = new Point(0, 0);
+            zedGraphControl11.IsShowPointValues = true;
+            zedGraphControl11.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 50);
+
+            zedGraphControl2.Location = new Point(0, 0);
+            zedGraphControl2.IsShowPointValues = true;
+            zedGraphControl2.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 50);
+
+            zedGraphControl3.Location = new Point(0, 0);
+            zedGraphControl3.IsShowPointValues = true;
+            zedGraphControl3.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 50);
+
+            zedGraphControl4.Location = new Point(0, 0);
+            zedGraphControl4.IsShowPointValues = true;
+            zedGraphControl4.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 50);
+
+            zedGraphControl5.Location = new Point(0, 0);
+            zedGraphControl5.IsShowPointValues = true;
+            zedGraphControl5.Size = new Size(this.ClientRectangle.Width - 20, this.ClientRectangle.Height - 50);
+        }
+
+        private void zedGraphControl2_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IndividualGraph_Load(object sender, EventArgs e)
+        {
+            plotGraph();
+            SetSize();
+        }
+
+        private void IndividualGraph_Resize(object sender, EventArgs e)
+        {
+            SetSize();
+        }
+    }
+}
