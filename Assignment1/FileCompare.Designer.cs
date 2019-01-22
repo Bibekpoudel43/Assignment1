cba9ca34f6623d97641e802c37b447909e7eed91
Bibek Paudel <bibek_poudel2@outlook.com>
2019-01-22 17:53:10 +0545

    added some functionalities

--- Assignment1/FileCompare.Designer.cs
+++ Assignment1/FileCompare.Designer.cs
@@ -30,41 +30,38 @@
         {
             this.file1txt = new System.Windows.Forms.TextBox();
             this.file2txt = new System.Windows.Forms.TextBox();
-            this.File2GridView = new System.Windows.Forms.DataGridView();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.file1btn = new System.Windows.Forms.Button();
             this.file2btn = new System.Windows.Forms.Button();
             this.File1GridView = new System.Windows.Forms.DataGridView();
             this.btnCompare = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.File2GridView)).BeginInit();
+            this.btnSummaryGraph = new System.Windows.Forms.Button();
+            this.File2GridView = new System.Windows.Forms.DataGridView();
+            this.deviceName = new System.Windows.Forms.Label();
+            this.deviceName1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.File1GridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.File2GridView)).BeginInit();
             this.SuspendLayout();
             // 
             // file1txt
             // 
-            this.file1txt.Location = new System.Drawing.Point(12, 12);
+            this.file1txt.Location = new System.Drawing.Point(37, 20);
             this.file1txt.Name = "file1txt";
             this.file1txt.Size = new System.Drawing.Size(168, 20);
             this.file1txt.TabIndex = 2;
             // 
             // file2txt
             // 
-            this.file2txt.Location = new System.Drawing.Point(12, 38);
+            this.file2txt.Location = new System.Drawing.Point(37, 44);
             this.file2txt.Name = "file2txt";
             this.file2txt.Size = new System.Drawing.Size(168, 20);
             this.file2txt.TabIndex = 3;
             // 
-            // File2GridView
-            // 
-            this.File2GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.File2GridView.Location = new System.Drawing.Point(12, 292);
-            this.File2GridView.Name = "File2GridView";
-            this.File2GridView.Size = new System.Drawing.Size(585, 150);
-            this.File2GridView.TabIndex = 5;
-            // 
             // file1btn
             // 
-            this.file1btn.Location = new System.Drawing.Point(179, 12);
+            this.file1btn.Location = new System.Drawing.Point(204, 20);
             this.file1btn.Name = "file1btn";
             this.file1btn.Size = new System.Drawing.Size(82, 21);
             this.file1btn.TabIndex = 0;
@@ -74,7 +71,7 @@
             // 
             // file2btn
             // 
-            this.file2btn.Location = new System.Drawing.Point(179, 36);
+            this.file2btn.Location = new System.Drawing.Point(204, 44);
             this.file2btn.Name = "file2btn";
             this.file2btn.Size = new System.Drawing.Size(82, 22);
             this.file2btn.TabIndex = 1;
@@ -84,15 +81,18 @@
             // 
             // File1GridView
             // 
+            this.File1GridView.AccessibleDescription = "public";
+            this.File1GridView.AccessibleName = "File1GridView";
             this.File1GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.File1GridView.Location = new System.Drawing.Point(12, 93);
+            this.File1GridView.Location = new System.Drawing.Point(12, 101);
             this.File1GridView.Name = "File1GridView";
-            this.File1GridView.Size = new System.Drawing.Size(580, 150);
+            this.File1GridView.Size = new System.Drawing.Size(451, 160);
             this.File1GridView.TabIndex = 4;
+            this.File1GridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.File1GridView_CellClick);
             // 
             // btnCompare
             // 
-            this.btnCompare.Location = new System.Drawing.Point(289, 24);
+            this.btnCompare.Location = new System.Drawing.Point(314, 32);
             this.btnCompare.Name = "btnCompare";
             this.btnCompare.Size = new System.Drawing.Size(133, 22);
             this.btnCompare.TabIndex = 6;
@@ -100,13 +100,76 @@
             this.btnCompare.UseVisualStyleBackColor = true;
             this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
             // 
+            // btnSummaryGraph
+            // 
+            this.btnSummaryGraph.Location = new System.Drawing.Point(330, 280);
+            this.btnSummaryGraph.Name = "btnSummaryGraph";
+            this.btnSummaryGraph.Size = new System.Drawing.Size(133, 22);
+            this.btnSummaryGraph.TabIndex = 7;
+            this.btnSummaryGraph.Text = "View Graph";
+            this.btnSummaryGraph.UseVisualStyleBackColor = true;
+            this.btnSummaryGraph.Click += new System.EventHandler(this.btnSummaryGraph_Click);
+            // 
+            // File2GridView
+            // 
+            this.File2GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.File2GridView.Location = new System.Drawing.Point(12, 321);
+            this.File2GridView.Name = "File2GridView";
+            this.File2GridView.Size = new System.Drawing.Size(451, 160);
+            this.File2GridView.TabIndex = 8;
+            // 
+            // deviceName
+            // 
+            this.deviceName.AutoSize = true;
+            this.deviceName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deviceName.Location = new System.Drawing.Point(12, 79);
+            this.deviceName.Name = "deviceName";
+            this.deviceName.Size = new System.Drawing.Size(96, 19);
+            this.deviceName.TabIndex = 9;
+            this.deviceName.Text = "DeviceName";
+            // 
+            // deviceName1
+            // 
+            this.deviceName1.AutoSize = true;
+            this.deviceName1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deviceName1.Location = new System.Drawing.Point(12, 299);
+            this.deviceName1.Name = "deviceName1";
+            this.deviceName1.Size = new System.Drawing.Size(96, 19);
+            this.deviceName1.TabIndex = 10;
+            this.deviceName1.Text = "DeviceName";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(495, 183);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 222);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(569, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Difference of Each Selected Row";
+            // 
             // FileCompare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(877, 509);
-            this.Controls.Add(this.btnCompare);
+            this.ClientSize = new System.Drawing.Size(925, 509);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.deviceName1);
+            this.Controls.Add(this.deviceName);
             this.Controls.Add(this.File2GridView);
+            this.Controls.Add(this.btnSummaryGraph);
+            this.Controls.Add(this.btnCompare);
             this.Controls.Add(this.File1GridView);
             this.Controls.Add(this.file2txt);
             this.Controls.Add(this.file1txt);
@@ -114,8 +177,8 @@
             this.Controls.Add(this.file1btn);
             this.Name = "FileCompare";
             this.Text = "File_Compare";
-            ((System.ComponentModel.ISupportInitialize)(this.File2GridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.File1GridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.File2GridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,11 +188,16 @@
 
         private System.Windows.Forms.TextBox file1txt;
         private System.Windows.Forms.TextBox file2txt;
-        private System.Windows.Forms.DataGridView File2GridView;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button file1btn;
         private System.Windows.Forms.Button file2btn;
         private System.Windows.Forms.DataGridView File1GridView;
         private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnSummaryGraph;
+        private System.Windows.Forms.DataGridView File2GridView;
+        private System.Windows.Forms.Label deviceName;
+        private System.Windows.Forms.Label deviceName1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Assignment1/FileCompare.cs
+++ Assignment1/FileCompare.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZedGraph;
+
 namespace Assignment1
 {
     public partial class FileCompare : Form
@@ -25,10 +27,12 @@ namespace Assignment1
         List<int> powerbalance = new List<int>();
         List<DateTime> dateTime = new List<DateTime>();
         List<String> summary = new List<String>();
+        Dictionary<string, List<string>> hrData = new Dictionary<string, List<string>>();
+       
+
 
         List<string> parametersArray1 = new List<string>();
-        Dictionary<string, string> parameter1 = new Dictionary<string, string>(); List<string> arrays = new List<string>();
-        List<string> arrays1 = new List<string>();
+        Dictionary<string, string> parameter1 = new Dictionary<string, string>(); 
         List<int> heart11 = new List<int>();
         List<double> speed11 = new List<double>();
         List<double> speed_mile11 = new List<double>();
@@ -38,9 +42,10 @@ namespace Assignment1
         List<int> powerbalance11 = new List<int>();
         List<DateTime> dateTime1 = new List<DateTime>();
         List<String> summary1 = new List<String>();
+        Dictionary<string, List<string>> hrData1 = new Dictionary<string, List<string>>();
+
 
         char[] findOf = { '\t', ' ', '=' };
-        int speed1, cadence1, altitude1, power1, powerbal;
         DateTime dt = new DateTime();
         int heartCheck = 0;
         int speedCheck = 0;
@@ -107,7 +112,6 @@ namespace Assignment1
                     {
                         parametersArray.Add(val[i]);
                     }
-                    arrays.Add(line);
 
                 }
 
@@ -158,6 +162,9 @@ namespace Assignment1
                 Console.WriteLine(e);
             }
 
+            Form1 f = new Form1();
+            deviceName.Text = f.deviceN(parameter["Monitor"]);
+
         }
 
         public void fileprocessing1(string filePath)
@@ -170,11 +177,12 @@ namespace Assignment1
                 StreamReader sr = new StreamReader(filePath);
                 string line = "";
                 string newLine = "";
+                int counter1 = 0;
 
                 while (!(line = sr.ReadLine()).Contains("[Note]"))
                 {
 
-                    counter++;
+                    counter1++;
                     string newline = string.Join(" ", line.Split(findOf, StringSplitOptions.RemoveEmptyEntries));
                     List<string> val = newline.Split(' ').ToList();
 
@@ -182,7 +190,6 @@ namespace Assignment1
                     {
                         parametersArray1.Add(val[i]);
                     }
-                    arrays1.Add(line);
 
                 }
 
@@ -217,7 +224,6 @@ namespace Assignment1
 
                             sortDataIntoArray1(newLine);
 
-
                         }
                     }
                 }
@@ -233,6 +239,9 @@ namespace Assignment1
                 Console.WriteLine(e);
             }
 
+            Form1 fm = new Form1();
+            deviceName1.Text = fm.deviceN(parameter1["Monitor"]);
+
         }
 
         public void sortDataIntoArray(string line)
@@ -240,6 +249,7 @@ namespace Assignment1
             try
             {
                 int heart11 = 0;
+                int speed1 = 0, cadence1 = 0, altitude1 = 0, power1 = 0, powerbal = 0;
                 string newline = string.Join(" ", line.Split(findOf, StringSplitOptions.RemoveEmptyEntries));
                 List<string> val = newline.Split(' ').ToList();
                 if (heartCheck == 1)
@@ -286,39 +296,39 @@ namespace Assignment1
         {
             try
             {
-                int heart1 = 0;
+                int hrt = 0, sp = 0, cd = 0, al = 0, pw = 0, pwb = 0;
                 string newline = string.Join(" ", line.Split(findOf, StringSplitOptions.RemoveEmptyEntries));
                 List<string> val = newline.Split(' ').ToList();
                 if (heartCheck == 1)
                 {
-                    heart1 = int.Parse(val[0]);
+                    hrt = int.Parse(val[0]);
                 }
                 if (speedCheck == 1)
                 {
-                    speed1 = int.Parse(val[1]);
+                    sp = int.Parse(val[1]);
                 }
                 if (cadenceCheck == 1)
                 {
-                    cadence1 = int.Parse(val[2]);
+                    cd = int.Parse(val[2]);
                 }
                 if (powerCheck == 1)
                 {
-                    power1 = int.Parse(val[4]);
-                    powerbal = int.Parse(val[5]);
+                    pw = int.Parse(val[4]);
+                    pwb = int.Parse(val[5]);
 
                 }
                 if (altitudeCheck == 1)
                 {
-                    altitude1 = int.Parse(val[3]);
+                    al = int.Parse(val[3]);
                 }
 
-                heart11.Add(heart1);
-                speed11.Add(speed1 * 0.1);
-                speed_mile11.Add(speed1 * 0.1 * 0.62);
-                cadence11.Add(cadence1);
-                altitude11.Add(altitude1);
-                power11.Add(power1);
-                powerbalance11.Add(powerbal);
+                heart11.Add(hrt);
+                speed11.Add(sp * 0.1);
+                speed_mile11.Add(roundOff(sp * 0.1 * 0.621371));
+                cadence11.Add(cd);
+                altitude11.Add(al);
+                power11.Add(pw);
+                powerbalance11.Add(pwb);
                 val = null;
             }
             catch (Exception e)
@@ -327,7 +337,7 @@ namespace Assignment1
             }
         }
         //minimizing the decimal value to 2
-        private static double roundOff(double val)
+        public static double roundOff(double val)
         {
             double data = Math.Round(val, 2, MidpointRounding.AwayFromZero);
             return data;
@@ -356,10 +366,10 @@ namespace Assignment1
         private void btnCompare_Click(object sender, EventArgs e)
         {
             fileprocessing(file1txt.Text);
-            FilTable1();
+            FillTable1();
 
             fileprocessing1(file2txt.Text);
-            FilTable2();
+            FillTable2();
 
             //SummaryFiller();
 
@@ -367,7 +377,14 @@ namespace Assignment1
             //CreateIndividualGraph();
         }
 
-        public void FilTable1()
+        private void btnSummaryGraph_Click(object sender, EventArgs e)
+        {
+            //instansating summarygraph variable (display in combied graph)
+            FileCompareSummaryGraph sm = new FileCompareSummaryGraph(heart, speed, cadence, altitude, power, heart11, speed11, cadence11, altitude11, power11, parameter, parameter1);
+            sm.Show();
+        }
+
+        public void FillTable1()
         {
                 int counter = 0;
                 foreach (int value in heart)
@@ -386,18 +403,20 @@ namespace Assignment1
                 }
         }
 
-        public void FilTable2()
+
+        public void FillTable2()
         {
-                 int counter1 = 0;
-                foreach (int value in heart)
+            int interval = 0;
+            int counter1 = 0;
+                foreach (int value in heart11)
                 {
-                    File2GridView.Rows.Add(timeBuilder(parameter["StartTime"])
-                     , heart[counter]
-                     , speed[counter]
-                     , cadence[counter]
-                     , altitude[counter]
-                     , power[counter]
-                     , powerbalance[counter]
+                    File2GridView.Rows.Add(timeBuilder(parameter1["StartTime"])
+                     , heart11[counter1]
+                     , speed11[counter1]
+                     , cadence11[counter1]
+                     , altitude11[counter1]
+                     , power11[counter1]
+                     , powerbalance11[counter1]
                      );
                     counter1++;
                     interval++;
@@ -412,23 +431,110 @@ namespace Assignment1
             File1GridView.Columns[0].Name = "TimeInterval (HH:MM:SS)";
             File1GridView.Columns[1].Name = "Heart Rate (bpm)";
             File1GridView.Columns[2].Name = "Speed (km/h)";
-            File1GridView.Columns[3].Name = "Speed (mph)";
-            File1GridView.Columns[4].Name = "Cadence (rpm)";
-            File1GridView.Columns[5].Name = "Altitude (m)";
-            File1GridView.Columns[6].Name = "Power (watts)";
+            File1GridView.Columns[3].Name = "Cadence (rpm)";
+            File1GridView.Columns[4].Name = "Altitude (m)";
+            File1GridView.Columns[5].Name = "Power (watts)";
+            File1GridView.Columns[6].Name = "Power Balance";
         }
 
         //specifying column header
         private void InitGrid1()
         {
-            File2GridView.ColumnCount = 7;
+            File2GridView.ColumnCount =7;
             File2GridView.Columns[0].Name = "TimeInterval (HH:MM:SS)";
             File2GridView.Columns[1].Name = "Heart Rate (bpm)";
             File2GridView.Columns[2].Name = "Speed (km/h)";
-            File2GridView.Columns[3].Name = "Speed (mph)";
-            File1GridView.Columns[4].Name = "Cadence (rpm)";
-            File2GridView.Columns[5].Name = "Altitude (m)";
-            File2GridView.Columns[6].Name = "Power (watts)";
+            File2GridView.Columns[3].Name = "Cadence (rpm)";
+            File2GridView.Columns[4].Name = "Altitude (m)";
+            File2GridView.Columns[5].Name = "Power (watts)";
+            File2GridView.Columns[6].Name = "Power Balance";
         }
+
+        public int rowCount()
+        {
+            int count = 0;
+            count = File1GridView.RowCount;
+            return count;
+        }
+
+        public int rowCount1()
+        {
+            int count = 0;
+            count = File2GridView.RowCount;
+            return count;
+        }
+
+
+        private void File1GridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (File1GridView.SelectedRows.Count == 0 || File1GridView.SelectedRows.Count > 1)
+            {
+                MessageBox.Show("Select a Row");
+            }
+            else
+            {
+                String[] heart = null, speed = null, cadence = null, altitude = null, power = null, powerBalance = null;
+
+                String[] heart1 = null, speed1 = null, cadence1 = null, altitude1 = null, power1 = null, powerBalance1 = null;
+
+                File2GridView.ClearSelection();
+                File2GridView.Rows[e.RowIndex].Selected = true;
+
+                foreach (DataGridViewRow row in File1GridView.SelectedRows)
+                {
+                    heart = row.Cells[1].Value.ToString().Split(' ');
+                    speed = row.Cells[2].Value.ToString().Split(' ');
+                    cadence = row.Cells[3].Value.ToString().Split(' ');
+                    altitude = row.Cells[4].Value.ToString().Split(' ');
+                    power = row.Cells[5].Value.ToString().Split(' ');
+                    powerBalance = row.Cells[6].Value.ToString().Split(' ');
+                }
+
+                foreach (DataGridViewRow row in File2GridView.SelectedRows)
+                {
+                    heart1 = row.Cells[1].Value.ToString().Split(' ');
+                    speed1 = row.Cells[2].Value.ToString().Split(' ');
+                    cadence1 = row.Cells[3].Value.ToString().Split(' ');
+                    altitude1 = row.Cells[4].Value.ToString().Split(' ');
+                    power1 = row.Cells[5].Value.ToString().Split(' ');
+                    powerBalance1 = row.Cells[6].Value.ToString().Split(' ');
+                }
+
+                double heartNum = double.Parse(heart[0]);
+                double speedNum = double.Parse(speed[0]);
+                double cadenceNum = double.Parse(cadence[0]);
+                double altitudeNum = double.Parse(altitude[0]);
+                double powerNum = double.Parse(power[0]);
+                double powerbalanceNum = double.Parse(powerBalance[0]);
+
+                double heartNum1 = double.Parse(heart1[0]);
+                double speedNum1 = double.Parse(speed1[0]);
+                double cadenceNum1 = double.Parse(cadence1[0]);
+                double altitudeNum1 = double.Parse(altitude1[0]);
+                double powerNum1 = double.Parse(power1[0]);
+                double powerbalanceNum1 = double.Parse(powerBalance1[0]);
+
+                //String header=dataGridView2=
+                string env = Environment.NewLine;
+                textBox1.Text = "Difference In " + env +  env +
+                    "Heart Rate: " + heartNum + " - " + heartNum1 + " =  " + CalculateDifference(heartNum, heartNum1) +
+                    env + "Speed: " + speedNum + " - " + speedNum1 + " =  " + CalculateDifference(speedNum, speedNum1) +
+                    env + "Cadence: " + cadenceNum + " - " + cadenceNum1 + " =  " + CalculateDifference(cadenceNum, cadenceNum1) +
+                    env + "Altitude: " + altitudeNum + " - " + altitudeNum1 + " =  " + CalculateDifference(altitudeNum, altitudeNum1) +
+                    env + "Power: " + powerNum + " - " + powerNum1 + " =  " + CalculateDifference(powerNum, powerNum1) +
+                    env + "Power Balance: " + powerbalanceNum + " - " + powerbalanceNum1 + " =  " + CalculateDifference(powerbalanceNum, powerbalanceNum1) +
+                    env;
+
+            }
+        }
+        //calculate difference
+        public string CalculateDifference(double num1, double num2)
+        {
+            double rst = 0;
+            rst = num1 - num2;
+            return rst.ToString();
+        }
+
+
     }
 }
